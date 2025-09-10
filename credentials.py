5a5feec950b50c4cca8953b175af131ffad43626
DEEPAK PRINCE W <wdeepakprince@gmail.com>
2025-09-10 07:56:50 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,15 @@
+#import mysql.connector
+
+#mysql details
+host = 'localhost'
+username = 'root'
+password =''
+charset='utf8'
+
+#user-defined details
+database = 'bookeasy'
+table_user ='user'
+table_flights ='flights'
+table_bookings ='bookings'
+
+sqlquery=""
